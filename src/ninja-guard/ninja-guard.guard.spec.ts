48d3253fa458f03d9abb9940afba2a2ff88d8d00
abdellahabd <99611751+abdellahabd@users.noreply.github.com>
2024-06-24 10:12:37 +0100

    add Guard

---
+++ src/ninja-guard/ninja-guard.guard.spec.ts
@@ -0,0 +1,7 @@
+import { NinjaGuardGuard } from './ninja-guard.guard';
+
+describe('NinjaGuardGuard', () => {
+  it('should be defined', () => {
+    expect(new NinjaGuardGuard()).toBeDefined();
+  });
+});
