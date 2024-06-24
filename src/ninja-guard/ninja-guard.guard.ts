fe0ed02db7b5881aac7425de4b0a86813a3819ff
abdellahabd <99611751+abdellahabd@users.noreply.github.com>
2024-06-24 10:12:57 +0100

    add Guard

--- src/ninja-guard/ninja-guard.guard.ts
+++ src/ninja-guard/ninja-guard.guard.ts
@@ -6,6 +6,8 @@ export class NinjaGuardGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    //if(request)
     return true;
   }
 }
